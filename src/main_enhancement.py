ffe132ee38b59439c2dd25cac5b08fe8fa99de46
Pham Tuan Minh <minh@coolielab.com>
2019-10-08 15:44:48 +0700

    make project compatible python 3

--- src/main_enhancement.py
+++ src/main_enhancement.py
@@ -6,8 +6,11 @@ Created on Mon Apr 18 11:42:58 2016
 """
 
 import numpy as np
+import imageio
+import skimage
+from skimage.util import img_as_uint
 #import cv2
-#import numpy as np;
+import numpy as np;
 import matplotlib.pylab as plt;
 import scipy.ndimage
 import sys
@@ -17,11 +20,11 @@ from image_enhance import image_enhance
 
 if(len(sys.argv)<2):
     print('loading sample image');
-    img_name = '1.jpg'
-    img = scipy.ndimage.imread('../images/' + img_name);
+    img_name = '2.jpg'
+    img = imageio.imread('../images/' + img_name);
 elif(len(sys.argv) >= 2):
     img_name = sys.argv[1];
-    img = scipy.ndimage.imread(sys.argv[1]);
+    img = imageio.imread(sys.argv[1]);
     
 if(len(img.shape)>2):
     # img = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
@@ -30,16 +33,16 @@ if(len(img.shape)>2):
 rows,cols = np.shape(img);
 aspect_ratio = np.double(rows)/np.double(cols);
 
-new_rows = 350;             # randomly selected number
+new_rows = 800;             # randomly selected number
 new_cols = new_rows/aspect_ratio;
 
 #img = cv2.resize(img,(new_rows,new_cols));
-img = scipy.misc.imresize(img,(np.int(new_rows),np.int(new_cols)));
+img = skimage.transform.resize(img,(np.int(new_rows),np.int(new_cols)));
 
 enhanced_img = image_enhance(img);    
 
 if(1):
     print('saving the image')
-    scipy.misc.imsave('../enhanced/' + img_name,enhanced_img);
+    imageio.imwrite('../enhanced/' + img_name,img_as_uint(enhanced_img));
 else:
     plt.imshow(enhanced_img,cmap = 'Greys_r');
